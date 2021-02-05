5757e3afa823810c5d20b6f83da761bc5bd9cc1e
0x30c4 <abdullahsanaf@gmail.com>
2021-02-05 20:54:03 +0600

    .

--- yt/ytdl.py
+++ yt/ytdl.py
@@ -2,19 +2,15 @@ import youtube_dl
 
 ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
 
-with ydl:
-    result = ydl.extract_info(
-        'http://www.youtube.com/watch?v=BaW_jenozKc',
-        download=False # We just want to extract the info
-    )
-
-if 'entries' in result:
-    # Can be a playlist or a list of videos
-    video = result['entries'][0]
-else:
-    # Just a video
-    video = result
-
-print(video)
-video_url = video['url']
-print(video_url)
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }]
+    # 'logger': MyLogger(),
+    # 'progress_hooks': [my_hook],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
